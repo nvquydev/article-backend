7cd1bb35fae687ca33dd2dc3d7c4c9e514840629
VOTHANHNHAN\ADMIN <vtnhan241099@gmail.com>
2023-11-06 19:56:17 +0700

    apply swagger

--- src/main/java/com/quynv20/articlebackend/ArticleBackendApplication.java
+++ src/main/java/com/quynv20/articlebackend/ArticleBackendApplication.java
@@ -1,11 +1,24 @@
 package com.quynv20.articlebackend;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ArticleBackendApplication extends SpringBootServletInitializer {
 
 	@Override
@@ -16,4 +29,24 @@ public class ArticleBackendApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(ArticleBackendApplication.class, args);
 	}
+	
+	  @Bean
+	    public Docket api() {
+	        return new Docket(DocumentationType.SWAGGER_2)
+	                .select()
+	                .apis(RequestHandlerSelectors.basePackage("in.niraj.spring.springbootswagger.controller"))
+	                .paths(PathSelectors.any())
+	                .build()
+	                .apiInfo(apiInfo());
+	    }
+
+	    private ApiInfo apiInfo() {
+	        return new ApiInfo(
+	                "Online Store Api List",
+	                "Description of API",
+	                "API V1",
+	                "Terms of service",
+	                new Contact("Niraj Darji", "https://github.com/darjiniraj", "niraj.darji107@gmail.com"),
+	                "License of API", "ttps://localhost:8080", Collections.emptyList());
+	    }
 }
